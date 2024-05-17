e0e777e33c918fb9f2fed413a227c9cb54a55d4b
Mamadou LO <mamadou.lo@ratp.fr>
2024-05-17 16:37:17 +0200

    UPDATE files for Day 6

--- day6Functions/Functions.py
+++ day6Functions/Functions.py
@@ -2,4 +2,10 @@ def my_func():
     print("Hi")
     print("Hello")
 
+
+def my_function(name):
+    print(len(name))
+
+
 my_func()
+my_function("Hello")
---
+++ day6Functions/WhileLoop.py
@@ -0,0 +1,5 @@
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    print("Jump")
+    number_of_hurdles -= 1
+    print(number_of_hurdles)
