419e340290507985b72df11bfe467be1e4f1e389
Mamadou LO <mamadou.lo@ratp.fr>
2024-05-27 17:57:37 +0200

    ADD files for Day 12

---
+++ day12GuessingGame/Scoce.py
@@ -0,0 +1,19 @@
+################### Scope ####################
+
+enemies = 1
+
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+# Local scope
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+
+drink_potion()
